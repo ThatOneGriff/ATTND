816144b9e6cecf3231d1347179b841955d8a1de3
ThatOneGriff <temka.pervakov@gmail.com>
2025-08-31 12:08:51 +0300

    ⏳ Date math done

--- Components/Calendar/Date_Math.gd
+++ Components/Calendar/Date_Math.gd
@@ -5,13 +5,17 @@ class_name Date extends Node
 
 #region = PUBLIC =
 
-var day:   int = 0: set = _set_day
-var month: int = 0: set = _set_month
-var year:  int = 0: set = _set_year
+var day:   int = 0:
+	set = _set_day
+var month: int = 0:
+	set = _set_month
+var year:  int = 0:
+	set = _set_year
 
 
-### Constructor
-func _init(_day: int, _month: int, _year: int) -> void:
+### Ctor
+func _init(_day: int = 0, _month: int = 0, _year: int = 0) -> void:
+	### TODO: setting it based on current date
 	### Day setting depends on month, month setting depends on year.
 	### Thus, we set them in reverse order.
 	year  = _year
@@ -19,8 +23,8 @@ func _init(_day: int, _month: int, _year: int) -> void:
 	day   = _day
 
 
-### DD.MM.YYYY
-func output() -> void:
+## Return formatted DD.MM.YYYY
+func dd_mm_yyyy() -> String:
 	var day_str:   String = ""
 	var month_str: String = ""
 	
@@ -32,17 +36,12 @@ func output() -> void:
 		month_str = "0"
 	month_str += str(month)
 	
-	print(day_str + "." + month_str + "." + str(year))
+	return day_str + "." + month_str + "." + str(year)
 
 #endregion
 
 
 #region = PRIVATE =
-
-### TODO: Here's what I noticed.
-### When adding 61 days, do we skip 1 or 2 months?
-### While it's fairly straightforward with years, I guess adding days should come in steps.
-### Oh, and don't forget leap years.
 
 var _month_length: int = 0
 
@@ -50,15 +49,26 @@ var _month_length: int = 0
 ### 'new_value' is the value **that we're trying to set**,
 ### not what we add or substract from it.
 
+### 'day' setter; NOT MEANT TO BE USED EXTERNALLY
 func _set_day(new_value: int) -> void:
 	if (month == 0 or _month_length == 0):
 		push_error("Month & month length must be set first!")
 		get_tree().quit()
 	
-	if (new_value > _month_length):
-		pass
+	### If increased
+	while (new_value > _month_length): ### 1 month at a time
+		new_value -= _month_length
+		month += 1
+		day = new_value ### To not wrap 'while's in 'if's.
+	
+	### If decreased
+	while (new_value < -_month_length): ### 1 month at a time
+		# Might be VERY bugged
+		month -= 1
+		day = _month_length - new_value ### To not wrap 'while's in 'if's.
 
 
+### 'month' setter, also sets '_month_length'; NOT MEANT TO BE USED EXTERNALLY
 func _set_month(new_value: int) -> void:
 	if (year == 0):
 		push_error("Year must be set first!")
@@ -69,12 +79,14 @@ func _set_month(new_value: int) -> void:
 		month = new_value % 12
 	elif (new_value < 1):
 		# Might be VERY bugged
-		year += -1 + new_value / 12   ### Resetting a month backwards like that is automatically at least -1 year
+		### NOTE: I don't like the '-=' part
+		year -= -1 + new_value / 12   ### Resetting a month backwards like that is automatically at least -1 year
 		month = 12 - (-new_value % 12) ### ('-' for correct '%') If we substracted a month and got 0, it's 12. If we got -13, it's 11.
 	else:
 		month = new_value
 	
 	_set_month_length()
+
 
 # 1  2  3  4  5  6  7 month
 # 31 xx 31 30 31 30 31
@@ -83,6 +95,8 @@ func _set_month(new_value: int) -> void:
 # 31 30 31 30 31
 ### Conc.: 1-7 matches 8-12 with exclusion of February,
 ### This lets us use maths and not a lookup table.
+
+### '_month_length' *calculator*; NOT MEANT TO BE USED EXTERNALLY
 func _set_month_length() -> void:
 	if (month == 0):
 		push_error("Month must be set first!")
@@ -105,6 +119,7 @@ func _set_month_length() -> void:
 	_month_length = 30 + (_month_math_substitute % 2)
 
 
+### 'year' setter; NOT MEANT TO BE USED EXTERNALLY
 func _set_year(new_value: int) -> void:
 	### Commented part raises a concern:
 	### it's just too easy to cause an error via normal usage.
